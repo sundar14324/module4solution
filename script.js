e7fdf85bad8496b60adeacb5430282dfc11b0da8
sundar14324 <91717189+sundar14324@users.noreply.github.com>
2021-11-02 00:18:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+var names = new Array();
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="Frank";
+names[6]="larryy";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
+
+for(var i=0;i<names.length;i++) {
+  if(n[i].chartAt(0)==="J" || n[i].chartAt(0)==="j") {
+      console.log("Goodbye"+ names[i])
+  }
+  else{
+    console.log("Hello"+ names[i])
+  }
+  }
+  
